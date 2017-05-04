8edeeca9f25f4a8220664ab1059b6080c5b03293
shuaihan <1034683568@qq.com>
2017-05-04 14:23:58 +0800

    InterruptThread

---
+++ src/chapter2/InterruptAndStopThread.java
@@ -0,0 +1,33 @@
+package chapter2;
+
+/**
+ * Created by shuaihan on 2017/5/4.
+ */
+public class InterruptAndStopThread {
+    public static void main(String args[]) throws InterruptedException {
+
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                while (true) {
+                    if (Thread.currentThread().isInterrupted()) {
+                        System.out.println("收到中断信号,停止该线程!");
+                        break;
+                    }
+                    try {
+                        Thread.sleep(600);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                        Thread.currentThread().interrupt();
+                    }
+                    System.out.println("Running!");
+                    Thread.yield();
+                }
+            }
+        };
+
+        thread.start();
+        Thread.sleep(2000);
+        thread.interrupt();//进行中断操作
+    }
+}
---
+++ src/chapter2/InterruptThread.java
@@ -0,0 +1,28 @@
+package chapter2;
+
+/**
+ * Created by shuaihan on 2017/5/4.
+ */
+public class InterruptThread {
+    public static void main(String args[]) throws InterruptedException {
+
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("Running!");
+                    Thread.yield();
+                }
+            }
+        };
+
+        thread.start();
+        Thread.sleep(2000);
+        thread.interrupt();//进行中断操作,但是此操作没有任何影响
+    }
+}
