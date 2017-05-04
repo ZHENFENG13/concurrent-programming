65b5aa2d8754d9e862d24074f1d5f336dea7e4d3
shuaihan <1034683568@qq.com>
2017-05-04 14:26:52 +0800

    join

--- src/chapter2/JoinMain.java
+++ src/chapter2/JoinMain.java
@@ -23,6 +23,7 @@ public class JoinMain {
         AddThread at = new AddThread();
         at.start();
         at.join();//使用join()方法后,主线程会等待AddThread执行完毕,i输出为1000000,如果没有这条语句,i输出为0
+        //可以查看join的底层代码,本质即让调用线程在当前线程对象实例上等待
         System.out.println(i);
     }
 }
